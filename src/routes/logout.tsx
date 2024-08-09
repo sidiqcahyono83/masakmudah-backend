53fb685f3d29d4bd10b1ec277a91687e96c33643
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-08-09 23:26:08 +0700

    create app

---
+++ src/routes/logout.tsx
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+import { checkUserToken } from "../midleware/cekUserToken";
+import { HonoApp } from "..";
+
+const app = new Hono<HonoApp>();
+
+app.get("/", checkUserToken, async (c) => {
+	return c.json({
+		message: "Logout",
+	});
+});
+
+export default app;
